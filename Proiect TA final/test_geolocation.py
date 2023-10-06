2a0d5a50753844286c4910c4384d50e0d607f0a8
Lavinia <114360870+Lavinia-M@users.noreply.github.com>
2023-10-06 17:05:21 +0300

    Update test_geolocation.py

--- Proiect TA final/test_geolocation.py
+++ Proiect TA final/test_geolocation.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium import webdriver
 from unittest import TestCase
 from selenium.webdriver.common.by import By
@@ -15,7 +13,7 @@ class Geolocation(TestCase):
         self.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.browser.get(self.BASE_URL)
         self.browser.maximize_window()
-        self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(5)
         geolocation_link = self.browser.find_element(*self.GEOLOCATION_SELECTOR)
         geolocation_link.click()
 
@@ -24,7 +22,5 @@ class Geolocation(TestCase):
 
     def test_where_am_i(self):
         self.browser.find_element(By.TAG_NAME, 'button').click()
-        time.sleep(1)
         self.browser.find_element(By.ID, "map-link").click()
-        time.sleep(2)
 
